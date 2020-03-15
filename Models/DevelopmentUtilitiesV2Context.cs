b8cf35ee62c7468bc66f2e9e775a1a029eefea23
Christopher Henry Simmons <Christopher.Simmons@protonmail.ch>
2020-03-15 20:44:50 +1300

    Changed connection string(s) to use new DB location and User.

--- Models/DevelopmentUtilitiesV2Context.cs
+++ Models/DevelopmentUtilitiesV2Context.cs
@@ -26,7 +26,7 @@ namespace DevelopmentUtilitiesV2RESTful.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-GIV9OCS\\MSSQLSERVER01;Initial Catalog=DevelopmentUtilitiesV2;User ID=RestfulAPIUser;Password=Password_21600681;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-GIV9OCS;Initial Catalog=DevelopmentUtilitiesV2;User ID=DevUtilitiesUser;Password=Password_21600681;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
